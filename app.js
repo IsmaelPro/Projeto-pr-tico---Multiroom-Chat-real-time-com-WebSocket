b5a1a9e1095cf5913bf33c0cd2a23b16beb3d350
Ismael Santos <ismael496@yahoo.com.br>
2020-08-06 10:52:17 -0300

    Update relacao de part na conversa

--- app.js
+++ app.js
@@ -22,12 +22,22 @@ io.on('connection', function(socket){
     });
 
     socket.on('msgParaServidor', function(data){
+
+        /*dialogo */
         socket.emit('msgParaCliente', 
         {apelido: data.apelido, mensagem: data.mensagem});
 
         socket.broadcast.emit('msgParaCliente', 
         {apelido: data.apelido, mensagem: data.mensagem});
 
+        /*participantes */
+        if(parseInt(data.apelido_atualizado) == 0){
+            socket.emit('participantesParaCliente', 
+            {apelido: data.apelido});
 
+            socket.broadcast.emit('participantesParaCliente', 
+            {apelido: data.apelido}
+            );
+        }
     });
 });
